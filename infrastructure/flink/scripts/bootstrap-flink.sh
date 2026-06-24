10d06906f3526848d93d79b0a3c29361eb48c1f2
hungfnguyen <hungfnguyen.de@gmail.com>
2026-06-24 22:16:39 +0700

    fix sh file

--- infrastructure/flink/scripts/bootstrap-flink.sh
+++ infrastructure/flink/scripts/bootstrap-flink.sh
@@ -21,20 +21,10 @@ fi
 mkdir -p "${RUNTIME_CONF_DIR}"
 cp "${SOURCE_CONF_DIR}"/* "${RUNTIME_CONF_DIR}/"
 
-python3 - <<'PY' "${SOURCE_CONF_DIR}/flink-conf.yaml" "${RUNTIME_CONF_DIR}/flink-conf.yaml" "${CHECKPOINTS_URI}" "${SAVEPOINTS_URI}"
-import pathlib
-import sys
-
-source_path = pathlib.Path(sys.argv[1])
-target_path = pathlib.Path(sys.argv[2])
-checkpoints = sys.argv[3]
-savepoints = sys.argv[4]
-
-content = source_path.read_text(encoding="utf-8")
-content = content.replace("__FLINK_CHECKPOINTS_URI__", checkpoints)
-content = content.replace("__FLINK_SAVEPOINTS_URI__", savepoints)
-target_path.write_text(content, encoding="utf-8")
-PY
+sed \
+  -e "s|__FLINK_CHECKPOINTS_URI__|${CHECKPOINTS_URI}|g" \
+  -e "s|__FLINK_SAVEPOINTS_URI__|${SAVEPOINTS_URI}|g" \
+  "${SOURCE_CONF_DIR}/flink-conf.yaml" > "${RUNTIME_CONF_DIR}/flink-conf.yaml"
 
 export FLINK_CONF_DIR="${RUNTIME_CONF_DIR}"
 export LOG4J_CONFIGURATION_FILE="${RUNTIME_CONF_DIR}/log4j2.properties"
